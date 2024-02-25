9a4d2b515ed7207b6a63c0d902627b22cd6a040f
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2024-02-25 21:08:39 +0100

    Add addProduct and removeProduct actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { addProduct, removeProduct } from './redux/productsSlice';
+
 function App() {
-  return <div>Test</div>;
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <div>{product.name}</div>
+              <div>{product.price}</div>
+              <div>{product.description}</div>
+              <div>
+                <button onClick={() => dispatch(removeProduct(product.id))}>
+                  remove
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={() => dispatch(addProduct())}>add</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- client/src/redux/productsSlice.js
+++ client/src/redux/productsSlice.js
@@ -1,9 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialState = [
+  {
+    id: '1',
+    name: 'black thsirt',
+    price: '199',
+    description: 'ipsum',
+    amount: 5,
+    img: '',
+  },
+  {
+    id: '2',
+    name: 'blue jeans',
+    price: '299',
+    description: 'lorem',
+    amount: 10,
+    img: '',
+  },
+];
 
 const productsSlice = createSlice({
   name: 'products',
-  initialState: [],
-  reducers: {},
+  initialState,
+  reducers: {
+    getProduct: (state) => {},
+    addProduct: (state) => {
+      state.push({ id: uuidv4(), name: 'xd' });
+    },
+    editProduct: (state) => {},
+    removeProduct: (state, target) => {
+      return state.filter((product) => product.id !== target.payload);
+    },
+  },
 });
+
+export const { addProduct, removeProduct } = productsSlice.actions;
 
 export default productsSlice.reducer;
