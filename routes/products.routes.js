d18f9150e17e18b5f72b729b71cbae10daa47975
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2024-04-02 22:18:47 +0200

    Add uploadImage utility function, rewrite delete controller

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/products.controller');
-const multer = require('multer');
-const upload = multer({ dest: './public/uploads' });
+const uploadImage = require('../utils/uploadImage');
 
 router.get('/products', productsController.getAll);
 router.get('/products/:id', productsController.getById);
 router.delete('/products/:id', productsController.delete);
-router.post('/add', upload.single('uploaded_file'), productsController.add);
+router.post(
+  '/add',
+  uploadImage.single('uploaded_file'),
+  productsController.add
+);
 
 module.exports = router;
---
+++ utils/uploadImage.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const PRODUCT_IMAGE_MAX_SIZE = require('../backendConfig');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads');
+  },
+  filename: (req, file, cb) => {
+    const [name, ext] = file.originalname.split('.');
+    cb(null, `${name}-${Date.now()}.${ext}`);
+  },
+});
+
+const uploadImage = multer({
+  storage,
+  limits: { fileSize: PRODUCT_IMAGE_MAX_SIZE },
+});
+
+module.exports = uploadImage;
