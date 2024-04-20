55b71295e97dbc4108428e0160cabb95c5e53aa2
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2024-04-20 23:46:42 +0200

    Add tests for products GET methods

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(productsRoutes);
 
-app.listen(8001, () => {
-  console.log('server is running...');
+const server = app.listen(8001, () => {
+  if (NODE_ENV === 'test') {
+    console.log('server is running...');
+  }
 });
 
 app.get('*', (req, res) => {
@@ -27,14 +29,28 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-mongoose.connect(process.env.DB_URI);
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'production') {
+  dbUri = process.env.DB_URI;
+} else if (NODE_ENV === 'test') {
+  dbUri = 'mongodb://localhost:27017/shopOne';
+} else {
+  dbUri = 'mongodb://localhost:27017/shopOne';
+}
+mongoose.connect(dbUri);
 
 const db = mongoose.connection;
 
 db.once('open', () => {
-  console.log('connected to the database');
+  if (NODE_ENV !== 'test') {
+    console.log('connected to the database');
+  }
 });
 
 db.on('error', (err) => {
   console.log('Error:' + err);
 });
+
+module.exports = server;
---
+++ test/api/product/get.test.js
@@ -0,0 +1,52 @@
+const Product = require('../../../models/productsModel.js');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../server.js');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('GET /products', () => {
+  before(async () => {
+    const testProduct = new Product({
+      _id: '6624327aca894b49e9f99066',
+      name: 'test',
+      price: 12,
+      amount: 2,
+      description: 'test test test test test test',
+      img: 'avatar.jpg',
+      date: '2024-04-04',
+    });
+    await testProduct.save();
+    const anotherTestProduct = new Product({
+      _id: '6624327aca894b49e9f99067',
+      name: 'test2',
+      price: 22,
+      amount: 22,
+      description: '2test test test test test test',
+      img: 'avatar.jpg',
+      date: '2024-04-04',
+    });
+    await anotherTestProduct.save();
+  });
+
+  it('/ should return all products', async () => {
+    const res = await request(server).get('/products');
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body.length).to.be.equal(2);
+  });
+
+  it('/:id should return product by id ', async () => {
+    const res = await request(server).get('/products/6624327aca894b49e9f99067');
+    expect(res.status).to.be.equal(200);
+    expect(res.body).to.be.an('object');
+    expect(res.body).to.not.be.null;
+  });
+
+  after(async () => {
+    await Product.deleteMany();
+  });
+});
