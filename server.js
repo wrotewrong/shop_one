d75dcd1d5a3f98b05a36b54adcb91fb140a43f91
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2024-04-30 23:58:25 +0200

    Configure server to use passport lib and Google auth

--- server.js
+++ server.js
@@ -4,8 +4,36 @@ const productsRoutes = require('./routes/products.routes');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.AUTH_SERVER_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -15,7 +43,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(productsRoutes);
 
-const server = app.listen(8001, () => {
+const server = app.listen(8000, () => {
   if (NODE_ENV !== 'test') {
     console.log('server is running...');
   }
