d2a6f2ad5f790386dc76c3b782d5a7ec3d50ed08
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2025-02-10 22:22:45 +0100

    Add Cart component

--- client/src/components/pages/Cart/Cart.js
+++ client/src/components/pages/Cart/Cart.js
@@ -1,5 +1,38 @@
+import { useSelector } from 'react-redux';
+import ProductAmount from '../../features/ProductAmount/ProductAmount';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const cart = useSelector((state) => state.cart.cart);
+
+  const handleClear = () => {
+    console.log('clear');
+  };
+
+  const handleRemove = () => {
+    console.log('remove');
+  };
+
+  return cart.length === 0 ? (
+    <div>xd</div>
+  ) : (
+    <div>
+      {cart.map((cartProduct) => {
+        return (
+          <div key={cartProduct._id}>
+            <div>{cartProduct.name}</div>
+            <div>{cartProduct.amount}</div>
+            <div>{cartProduct.price * cartProduct.amount}</div>
+            <ProductAmount>{cartProduct}</ProductAmount>
+            <button onClick={handleRemove}>remove</button>
+          </div>
+        );
+      })}
+      <div>{`Total price:${cart.reduce(
+        (a, b) => a.amount * a.price + b.amount * b.price
+      )}`}</div>
+      <button onClick={handleClear}>clear the cart</button>
+    </div>
+  );
 };
 
 export default Cart;
